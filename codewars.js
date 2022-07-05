57f167084c2aad061edbfbf0416855a42cf1be44
Vit Onischenko <slonik0775@gmail.com>
2022-07-05 23:27:09 +0300

    Total amount of points

--- codewars.js
+++ codewars.js
@@ -1,30 +1,30 @@
-// Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+// Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
 
-// Your task is to process a string with "#" symbols.
+// For example: ["3:1", "2:2", "0:1", ...]
 
-// Examples
-// "abc#d##c"      ==>  "ac"
-// "abc##d######"  ==>  ""
-// "#######"       ==>  ""
-// ""              ==>  ""
+// Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
 
-function cleanString(s) {
-    let str = s;
-    for (let i = 0; i < s.length-1; i += 1) {
-      const idx = str.indexOf('#');
-      const splitStr = str.split(str.slice(idx-1, idx+1)).join('');
-      
-      if (idx === 0) {
-        str = str.substring(1, );
-        
-        if (str === '#') {
-          return '';
+// if x > y: 3 points
+// if x < y: 0 point
+// if x = y: 1 point
+// Notes:
+
+// there are 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function points(games) {
+    let i = 0;
+    let points = 0;
+    for (i; i < games.length; i++) {
+        let each = games[i].split(":");
+        if (each[0] > each[1]) {
+            points += 3;
+        } else if (each[0] == each[1]) {
+            points += 1;
+        } else {
+            points += 0;
         }
-        
-      } else {
-        str = splitStr;
-      }
     }
-    
-    return str;
-  };
+    return points;
+}

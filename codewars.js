2255b8ec6140fe127e0bb83174a549d4427ea465
Vit Onischenko <slonik0775@gmail.com>
2022-07-05 22:44:31 +0300

    Backspaces in string

--- codewars.js
+++ codewars.js
@@ -1,6 +1,30 @@
-// Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
+// Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
 
+// Your task is to process a string with "#" symbols.
 
-String.prototype.digit = function() {
-    return this.match(/^[0-9]$/) ? true : false;
+// Examples
+// "abc#d##c"      ==>  "ac"
+// "abc##d######"  ==>  ""
+// "#######"       ==>  ""
+// ""              ==>  ""
+
+function cleanString(s) {
+    let str = s;
+    for (let i = 0; i < s.length-1; i += 1) {
+      const idx = str.indexOf('#');
+      const splitStr = str.split(str.slice(idx-1, idx+1)).join('');
+      
+      if (idx === 0) {
+        str = str.substring(1, );
+        
+        if (str === '#') {
+          return '';
+        }
+        
+      } else {
+        str = splitStr;
+      }
+    }
+    
+    return str;
   };

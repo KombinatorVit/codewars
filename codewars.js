1ab0383d00546fc0cfa5ded7205fd0f1d7be007d
Vit Onischenko <slonik0775@gmail.com>
2022-07-06 09:22:08 +0300

    a wolf

--- codewars.js
+++ codewars.js
@@ -1,15 +1,8 @@
-// You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
-
-// You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
-
-// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
-
-function mergeArrays(arr1, arr2) {
-    let arr = arr1.concat(arr2).sort((a, b) => a - b);
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === arr[i + 1]) {
-        arr.splice(i, 1);
+function warnTheSheep(queue) {
+    if (queue[queue.length -1] === 'wolf') {
+      return 'Pls go away and stop eating my sheep';
+      } else {
+       let index = queue.findIndex( (x) => x == 'wolf' );
+       return `Oi! Sheep number ${queue.length - index - 1}! You are about to be eaten by a wolf!`;
       }
-    }
-    return arr;
   }
